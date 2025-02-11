ef656b6676c2af2afa5001f8524f8dc450fffb8d
akshya <akshya@don.com>
2025-02-11 14:28:46 +0530

    booom

---
+++ src/firebase-comment.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { collection, addDoc } from "@firebase/firestore";
+
+const firebaseConfig = {
+    
+};
+
+// Initialize with a unique name
+const app = initializeApp(firebaseConfig, 'comments-app');
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { db, storage, collection, addDoc };
