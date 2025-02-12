b1f8cf36e06625b393b98f4c169baeed6fc06c00
Akshya Thapa <144545650+LowkeyGUY4444@users.noreply.github.com>
2025-02-12 09:46:53 +0530

    Update firebase.js  api key

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,13 @@ import { collection, addDoc, getDocs } from "@firebase/firestore"; // Perbarui i
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB-lfUt1adpQ0KYcFFW_oAWTJVfHDOOZy8",
-  authDomain: "portofolio-web-3e8e8.firebaseapp.com",
-  projectId: "portofolio-web-3e8e8",
-  storageBucket: "portofolio-web-3e8e8.appspot.com",
-  messagingSenderId: "25195509306",
-  appId: "1:25195509306:web:2b635dcf997137bf612703"
+apiKey: "AIzaSyC8xVxNTbC4AgadHBD2hOireM0juS9BWQQ",
+  authDomain: "protfolio-91755.firebaseapp.com",
+  projectId: "protfolio-91755",
+  storageBucket: "protfolio-91755.firebasestorage.app",
+  messagingSenderId: "6946674912",
+  appId: "1:6946674912:web:ffc08c8cfb84cc57135885",
+  measurementId: "G-PFVL81NFZF"
 };
 
 // Initialize Firebase
